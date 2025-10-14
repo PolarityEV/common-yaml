c9c48d80481302f99b3557cccbab1fb65518c9cb
Bastian de Byl <bastian@debyl.io>
2025-10-13 20:35:19 -0400

    chore: update VCU_I2C_CHARGER_CONTROL comment

--- generated/skudak_common.h
+++ generated/skudak_common.h
@@ -42,7 +42,7 @@
 #define VCU_I2C_VW_BMS_DEBUG           VCU_I2C_BMS_DIAGNOSTICS  ///< Legacy alias (deprecated, use VCU_I2C_BMS_DIAGNOSTICS)
 
 /* Charger Control & Monitoring Modes */
-#define VCU_I2C_CHARGER_CONTROL        (0x0C)  ///< Charger control (WRITE: 5 bytes: enable, max_voltage, max_current; RESPONSE: 1 byte: status)
+#define VCU_I2C_CHARGER_CONTROL        (0x0C)  ///< Charger control (WRITE: 6 bytes total [command + 5 data]; RESPONSE: 1 byte: 0=OK, 1=Error) [Implemented v0.3.1+]
 #define VCU_I2C_CHARGER1_OBC           (0x0D)  ///< Charger 1 OBC state (CAN1 bus) (READ: 16 bytes: voltage, current, status flags, temperature, timeout)
 #define VCU_I2C_CHARGER1_DCDC          (0x0E)  ///< Charger 1 DCDC state (CAN1 bus) (READ: 16 bytes: fault flags, output current, output voltage, output status, temperature, timeout)
 #define VCU_I2C_CHARGER2_OBC           (0x0F)  ///< Charger 2 OBC state (CAN3 bus) (READ: 16 bytes: same format as CHARGER1_OBC)
